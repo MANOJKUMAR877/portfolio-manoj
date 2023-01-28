553feaeb475b4ae8e03ed607e16aa2c491247379
Manojkumar Krishnaramanujam Baskaran <58929289+MANOJKUMAR877@users.noreply.github.com>
2023-01-28 15:51:56 +0100

    udpates

---
+++ src/Project.js
@@ -0,0 +1,146 @@
+import React from "react";
+import "./Project.css";
+import Project1 from "./img/HRMS.jpg";
+import Project2 from "./img/room.jpg";
+import Project3 from "./img/e1.jpeg";
+import Project4 from "./img/4.png";
+import Project5 from "./img/5.jpeg";
+import Project6 from "./img/6.jpeg";
+import Manojreport from "./img/Manojkumar_FinalYearReport.pdf";
+function Project() {
+  return (
+    <div className="project component__space" id="Projects">
+      <div className="heading">
+        <h1 className="heading">My Latest Project</h1>
+        <p className="heading p__color">Some of my projects to your eyes</p>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project1} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h4 className="project__text">
+                  Human Resource Management System
+                </h4>
+                {/* <a href={Manojreport} target="_blank">
+                  <button
+                    style={{ border: "none", width: "30rem", color: "red" }}
+                  >
+                    Click Here to download thesis
+                  </button>
+                </a> */}
+                {/* <p className="project__text">
+                  Getting tickets to the big show
+                </p> */}
+                {/* <a href="#" className="project__btn">
+                  View Details
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box" style={{ width: "100%" }}>
+                  <img src={Project3} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h4 className="project__text">E-Commerce Web App</h4>
+                {/* <a href="#" className="project__btn">
+                  View Details
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project2} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h4 className="project__text">Room Booking System</h4>
+
+                {/* <a href="#" className="project__btn">
+                  View Details
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project4} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h4 className="project__text">Online Quizz APP</h4>
+                {/* <a href="#" className="project__btn">
+                  View Details
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project5} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h4 className="project__text">Real Time Chat APP</h4>
+                {/* <a href="#" className="project__btn">
+                  View Details
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__box__img pointer relative">
+                <div className="project__img__box">
+                  <img src={Project6} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                {/* <h5 className="project__text"></h5> */}
+                <h4 className="project__text">
+                  Kinetic Temperature Sensing Robot
+                </h4>
+                {/* <a href="#" className="project__btn">
+                  View Details
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
+            <button className="view__more pointer btn">View more</button>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
---
+++ src/Service.js
@@ -0,0 +1,270 @@
+import React from "react";
+import "./Service.css";
+
+function Service() {
+  return (
+    <div className="service component__space" id="Resume">
+      <div className="heading">
+        <h1 className="heading">My Awesome Contributions</h1>
+        {/* <p className="heading p__color">
+          There are many variations of passages of Lorem Ipsum available,
+        </p>
+        <p className="heading p__color">
+          but the majority have suffered alteration.
+        </p> */}
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  Software Developer - ( HumanAize || Amicocon) - work student
+                </h1>
+                <h1 className="service__text">From (01/06/2022 - Till Date)</h1>
+                <p className="p service__text p__color top">
+                  Participate and provide some input in project code reviews.
+                  Runs software tests designed by more experienced engineers.
+                  Reviews test result and perform root cause analyses to
+                  identify problems. Make recommendations to the immediate team
+                  to enhance performance productivity.
+                </p>
+                <p className="p service__text p__color top">
+                  Present architectural recommendations to the Architecture
+                  Review Board. Work with QA to ensure the quality of the
+                  features, including the creation and maintenance of test
+                  cases.
+                </p>
+                <p className="p service__text p__color top">
+                  Own a feature through the development life-cycle, from
+                  requirements gathering to implementation, testing, and
+                  post-release support. Proactively Identify risks and failure
+                  modes early in the development lifecycle and develop POCs to
+                  mitigate the risks early in the program
+                </p>
+
+                <p className="p service__text p__color top">
+                  Update the daily tasks in close collaboration with the CTO,
+                  and participate with him in planning the upcoming sprint and
+                  UI components{" "}
+                </p>
+
+                {/* <p className="p service__text p__color">close to the earth.</p> */}
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  Software Developer - (Kyra Technology by PRLX WEB)
+                </h1>
+                <h1 className="service__text">
+                  From (02/06/2021 - 31/01/2022)
+                </h1>
+                <p className="p service__text p__color top">
+                  Researching, designing, implementing, and managing software
+                  programs Testing and evaluating new programs. Identifying
+                  areas for modification in existing programs and subsequently
+                  developing these modifications Design highly scalable,
+                  testable code. Fix programming bugs. • Participate and provide
+                  some input in project code reviews.
+                </p>
+                <p className="p service__text p__color top">
+                  Runs software tests designed by more experienced engineers.
+                  Reviews test result and perform root cause analyses to
+                  identify problems. Make recommendations to the immediate team
+                  to enhance performance productivity.
+                </p>
+                {/* <p className="p service__text p__color">close to the earth.</p> */}
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  Associate Software Developer - (BlackGold Innovation Pvt Ltd)
+                </h1>
+                <h1 className="service__text">
+                  From (04/05/2020) To (31/05/2021)
+                </h1>
+                <p className="p service__text p__color">
+                  Developing the Admin portal for Ecomm verse web application in
+                  which we can manage various identity services policies. Access
+                  tools for managing in the areas of authentication, orders and
+                  fulfilling, products, customers, analytics, marketing, etc.
+                </p>
+                <p className="p service__text p__color">
+                  Actively participate in technical innovations, external
+                  presentations. Experience in managing projects across cross
+                  teams, building sustainable processes, and coordinating
+                  release schedules.
+                </p>
+              </div>
+            </div>
+          </div> */}
+
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  Associate Software Engineer Intern - (Innoventes Technologies)
+                </h1>
+                <h1 className="service__text">
+                  From (16/12/2019) To (31/03/2020)
+                </h1>
+                <p className="p service__text p__color top">
+                  Develop the end to end front end aspect for the project called
+                  Daily sales Report with the following technologies React js,
+                  Ionic connecting with backend API
+                </p>
+              
+                <p className="p service__text p__color top">
+                  From start to finish in order to complete a typical Web design
+                  project. It divides and categorizes the work and then breaks
+                  these high-level sections into tasks and resources that can be
+                  used as a road map for a project.
+                </p>
+                {/* <p className="p service__text p__color">close to the earth.</p> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="9" cy="7" r="4"></circle>
+                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  BE - Computer Science and Engineering
+                </h1>
+                <p className="p service__text p__color">
+                  At Sri Ramakrishna Institute Of Technology - (From 2016-2020)
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="9" cy="7" r="4"></circle>
+                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
+                  M.Engg - Software Engineering for Industrial Application
+                </h1>
+                <p className="p service__text p__color">
+                  At Hof University of Applied Sciences - (From 2022-2024)
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
