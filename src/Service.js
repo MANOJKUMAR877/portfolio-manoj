9d0e4c0f8ffb5b7c95de958c4c10ca700c4cc70a
Manojkumar Krishnaramanujam Baskaran <58929289+MANOJKUMAR877@users.noreply.github.com>
2024-03-25 22:02:15 +0100

    updates reusme

--- src/Service.js
+++ src/Service.js
@@ -6,12 +6,12 @@ function Service() {
     <div className="service component__space" id="Resume">
       <div className="heading">
         <h1 className="heading">My Awesome Contributions</h1>
-        {/* <p className="heading p__color">
+        {/* <li className="heading p__color">
           There are many variations of passages of Lorem Ipsum available,
-        </p>
-        <p className="heading p__color">
+        </li>
+        <li className="heading p__color">
           but the majority have suffered alteration.
-        </p> */}
+        </li> */}
       </div>
 
       <div className="container">
@@ -37,37 +37,115 @@ function Service() {
               </div>
               <div className="service__meta">
                 <h1 className="service__text">
+                  Master Thesis & Full-Stack Developer Internship
+                </h1>
+                <h1 className="service__text">From (17/03/2023 - Till Date)</h1>
+
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
+                  Speard headed the development of a specialized application
+                  using Python and React tailored for the evaluation of UDI
+                  codes, ensuring compliance with MDR standards and regulations.
+                </li>
+
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
+                  Collaborated cross-functionally to troubleshoot industrial
+                  tablets integrated with Cumulocity IoT cloud platform using
+                  ThinEdge. cases.
+                </li>
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
+                  Actively involved in the implementation and evolution of
+                  Micro-frontend architecture within the organization,
+                  contributing to its development and refinement.
+                </li>
+
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
+                  Stay up to date on relevant technologies, plug into user
+                  groups, understand trends and opportunities that ensure we are
+                  using the best techniques and tools{" "}
+                </li>
+
+                {/* <li className="li service__text p__color">close to the earth.</li> */}
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">
                   Software Developer - ( HumanAize || Amicocon) - work student
                 </h1>
-                <h1 className="service__text">From (01/06/2022 - Till Date)</h1>
-                <p className="p service__text p__color top">
+                <h1 className="service__text">
+                  From (01/06/2022 - 28/02/2023)
+                </h1>
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Participate and provide some input in project code reviews.
                   Runs software tests designed by more experienced engineers.
                   Reviews test result and perform root cause analyses to
                   identify problems. Make recommendations to the immediate team
                   to enhance performance productivity.
-                </p>
-                <p className="p service__text p__color top">
+                </li>
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Present architectural recommendations to the Architecture
                   Review Board. Work with QA to ensure the quality of the
                   features, including the creation and maintenance of test
                   cases.
-                </p>
-                <p className="p service__text p__color top">
+                </li>
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Own a feature through the development life-cycle, from
                   requirements gathering to implementation, testing, and
                   post-release support. Proactively Identify risks and failure
                   modes early in the development lifecycle and develop POCs to
                   mitigate the risks early in the program
-                </p>
+                </li>
 
-                <p className="p service__text p__color top">
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Update the daily tasks in close collaboration with the CTO,
                   and participate with him in planning the upcoming sprint and
                   UI components{" "}
-                </p>
+                </li>
 
-                {/* <p className="p service__text p__color">close to the earth.</p> */}
+                {/* <li className="li service__text p__color">close to the earth.</li> */}
               </div>
             </div>
           </div>
@@ -95,23 +173,29 @@ function Service() {
                   Software Developer - (Kyra Technology by PRLX WEB)
                 </h1>
                 <h1 className="service__text">
-                  From (02/06/2021 - 31/01/2022)
+                  From (01/04/2020 - 31/03/2022)
                 </h1>
-                <p className="p service__text p__color top">
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Researching, designing, implementing, and managing software
                   programs Testing and evaluating new programs. Identifying
                   areas for modification in existing programs and subsequently
                   developing these modifications Design highly scalable,
                   testable code. Fix programming bugs. • Participate and provide
                   some input in project code reviews.
-                </p>
-                <p className="p service__text p__color top">
+                </li>
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Runs software tests designed by more experienced engineers.
                   Reviews test result and perform root cause analyses to
                   identify problems. Make recommendations to the immediate team
                   to enhance performance productivity.
-                </p>
-                {/* <p className="p service__text p__color">close to the earth.</p> */}
+                </li>
+                {/* <li className="li service__text p__color">close to the earth.</li> */}
               </div>
             </div>
           </div>
@@ -142,18 +226,18 @@ function Service() {
                 <h1 className="service__text">
                   From (04/05/2020) To (31/05/2021)
                 </h1>
-                <p className="p service__text p__color">
+                <li className="li service__text p__color">
                   Developing the Admin portal for Ecomm verse web application in
                   which we can manage various identity services policies. Access
                   tools for managing in the areas of authentication, orders and
                   fulfilling, products, customers, analytics, marketing, etc.
-                </p>
-                <p className="p service__text p__color">
+                </li>
+                <li className="li service__text p__color">
                   Actively participate in technical innovations, external
                   presentations. Experience in managing projects across cross
                   teams, building sustainable processes, and coordinating
                   release schedules.
-                </p>
+                </li>
               </div>
             </div>
           </div> */}
@@ -184,19 +268,25 @@ function Service() {
                 <h1 className="service__text">
                   From (16/12/2019) To (31/03/2020)
                 </h1>
-                <p className="p service__text p__color top">
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   Develop the end to end front end aspect for the project called
                   Daily sales Report with the following technologies React js,
                   Ionic connecting with backend API
-                </p>
-              
-                <p className="p service__text p__color top">
+                </li>
+
+                <li
+                  className="li service__text p__color top"
+                  style={{ "text-align": "left" }}
+                >
                   From start to finish in order to complete a typical Web design
                   project. It divides and categorizes the work and then breaks
                   these high-level sections into tasks and resources that can be
                   used as a road map for a project.
-                </p>
-                {/* <p className="p service__text p__color">close to the earth.</p> */}
+                </li>
+                {/* <li className="li service__text p__color">close to the earth.</li> */}
               </div>
             </div>
           </div>
@@ -225,9 +315,9 @@ function Service() {
                 <h1 className="service__text">
                   BE - Computer Science and Engineering
                 </h1>
-                <p className="p service__text p__color">
+                <li className="li service__text p__color">
                   At Sri Ramakrishna Institute Of Technology - (From 2016-2020)
-                </p>
+                </li>
               </div>
             </div>
           </div>
@@ -255,9 +345,9 @@ function Service() {
                 <h1 className="service__text">
                   M.Engg - Software Engineering for Industrial Application
                 </h1>
-                <p className="p service__text p__color">
+                <li className="li service__text p__color">
                   At Hof University of Applied Sciences - (From 2022-2024)
-                </p>
+                </li>
               </div>
             </div>
           </div>
